cc2350343abd9b18238fb8391974e85fac1d05aa
Rodrigo Pimenta <rodrigopimenta10@gmail.com>
2019-10-20 11:47:48 -0400

    Added additional development made in past

---
+++ pdo_connect.php
@@ -0,0 +1,15 @@
+<?php
+
+$dsn = "mysql:host=localhost;dbname=myDatabase;charset=utf8mb4";
+$options = [
+  PDO::ATTR_EMULATE_PREPARES   => false, // turn off emulation mode for "real" prepared statements
+  PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, //turn on errors in the form of exceptions
+  PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, //make the default fetch be an associative array
+];
+try {
+  $pdo = new PDO($dsn, "username", "password", $options);
+} catch (Exception $e) {
+  error_log($e->getMessage());
+  exit('Something weird happened'); //something a user can understand
+}
+?>
---
+++ testing.php
@@ -0,0 +1,2 @@
+<?php
+$db = new PDO('mysql:host=localhost;dbname=testdb;charset=utf8mb4', 'username', 'password');
